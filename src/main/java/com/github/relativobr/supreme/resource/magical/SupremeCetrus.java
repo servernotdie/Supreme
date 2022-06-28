dbb87e3351ff60d78e495d0ec50b7df7f7318cc0
ybw0014 <i@guizhanss.com>
2022-06-28 03:01:17 -0400

    Certus翻译

--- src/main/java/com/github/relativobr/supreme/resource/magical/SupremeCetrus.java
+++ src/main/java/com/github/relativobr/supreme/resource/magical/SupremeCetrus.java
@@ -1,5 +1,5 @@
 package com.github.relativobr.supreme.resource.magical;
-//老子不会翻Cetrus 摆烂啦
+
 import com.github.relativobr.supreme.util.SupremeItemStack;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import lombok.experimental.UtilityClass;
@@ -10,7 +10,7 @@ import org.bukkit.inventory.ItemStack;
 public class SupremeCetrus {
 
   public static final SlimefunItemStack CETRUS_LUX = new SupremeItemStack("SUPREME_CETRUS_LUX",
-      "92cb6e51c461e7359526bea5e06209cddde7c6469a819f3405cf0a038c159502", "&a光Cetrus", "", "&7超级光权杖",
+      "92cb6e51c461e7359526bea5e06209cddde7c6469a819f3405cf0a038c159502", "&a金行", "", "&7五行之一",
       "", "&3至尊魔法合成材料");
   public static final ItemStack[] RECIPE_CETRUS_LUX = {new ItemStack(Material.FLINT, 64),
       new ItemStack(Material.GLOWSTONE, 64), new ItemStack(Material.TORCH, 64), new ItemStack(Material.CAMPFIRE, 64),
@@ -19,8 +19,8 @@ public class SupremeCetrus {
       new ItemStack(Material.SHROOMLIGHT, 64)};
 
   public static final SlimefunItemStack CETRUS_VENTUS = new SupremeItemStack("SUPREME_CETRUS_VENTUS",
-      "b28f1c0c5092e12d33770df45c5845a9610886039b34abe93a16c5e942dfc8e4", "&a风Cetrus", "",
-      "&7超级风权杖", "", "&3至尊魔法合成材料");
+      "b28f1c0c5092e12d33770df45c5845a9610886039b34abe93a16c5e942dfc8e4", "&a木行", "",
+      "&7五行之一", "", "&3至尊魔法合成材料");
   public static final ItemStack[] RECIPE_CETRUS_VENTUS = {new ItemStack(Material.FEATHER, 64),
       new ItemStack(Material.LEATHER, 64), new ItemStack(Material.CHORUS_FRUIT, 64),
       new ItemStack(Material.COBBLESTONE, 64), new SlimefunItemStack(SupremeCore.CORE_OF_NATURE, 1),
@@ -28,8 +28,8 @@ public class SupremeCetrus {
       new ItemStack(Material.ENDER_PEARL, 16)};
 
   public static final SlimefunItemStack CETRUS_LUMIUM = new SupremeItemStack("SUPREME_CETRUS_LUMIUM",
-      "4b1cce22de19ed6727abc5e6c2d57864c871a44c956bbe2eb3960269b686b8b3", "&a土Cetrus", "",
-      "&7超级土权杖", "", "&3至尊魔法合成材料");
+      "4b1cce22de19ed6727abc5e6c2d57864c871a44c956bbe2eb3960269b686b8b3", "&a土行", "",
+      "&7五行之一", "", "&3至尊魔法合成材料");
   public static final ItemStack[] RECIPE_CETRUS_LUMIUM = {new ItemStack(Material.DIRT, 64),
       new SlimefunItemStack(SupremeCore.CORE_OF_BLOCK, 1), new ItemStack(Material.GRASS_BLOCK, 64),
       new SlimefunItemStack(SupremeCore.CORE_OF_LIFE, 1), new ItemStack(Material.OBSIDIAN, 64),
@@ -37,7 +37,7 @@ public class SupremeCetrus {
       new SlimefunItemStack(SupremeCore.CORE_OF_ALLOY, 1), new ItemStack(Material.NETHER_STAR, 64)};
 
   public static final SlimefunItemStack CETRUS_AQUA = new SupremeItemStack("SUPREME_CETRUS_AQUA",
-      "36161daa3589ec9c8187459ac36fd4dd2646c040678d3bfacb72a2210c6c801c", "&a水Cetrus", "", "&7超级水权杖",
+      "36161daa3589ec9c8187459ac36fd4dd2646c040678d3bfacb72a2210c6c801c", "&a水行", "", "&7五行之一",
       "", "&3至尊魔法合成材料");
   public static final ItemStack[] RECIPE_CETRUS_AQUA = {new ItemStack(Material.ICE, 64),
       new ItemStack(Material.LILY_PAD, 64), new ItemStack(Material.SALMON, 64), new ItemStack(Material.COD, 64),
@@ -46,8 +46,8 @@ public class SupremeCetrus {
       new ItemStack(Material.WATER_BUCKET, 1)};
 
   public static final SlimefunItemStack CETRUS_IGNIS = new SupremeItemStack("SUPREME_CETRUS_IGNIS",
-      "e672959028f274b379d430f068f0f15a4f793eac12afb94ae0b4e50cf895df0f", "&a火Cetrus", "",
-      "&7超级火权杖", "", "&3至尊魔法合成材料");
+      "e672959028f274b379d430f068f0f15a4f793eac12afb94ae0b4e50cf895df0f", "&a火行", "",
+      "&7五行之一", "", "&3至尊魔法合成材料");
   public static final ItemStack[] RECIPE_CETRUS_IGNIS = {new ItemStack(Material.NETHERRACK, 64),
       new ItemStack(Material.BLAZE_ROD, 64), new ItemStack(Material.SOUL_SAND, 64),
       new ItemStack(Material.QUARTZ_BLOCK, 64), new SlimefunItemStack(SupremeCore.CORE_OF_BLOCK, 1),
